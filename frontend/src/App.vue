40d4a9256abb33aab04e4fee616ef0744f108e57
linnamuth <muthlinna73gmail.com>
2023-07-04 16:29:31 +0700

    [G3SMS-41] FRONT-END: Implement password reset page.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<reset-password></reset-password>
+  
 </template>
 
+<script>
+import ResetPassword from './views/Login/ResetPassword.vue'
+
+export default {
+  components: { 
+    ResetPassword 
+  },
+  
+
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
