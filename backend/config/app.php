43985278a5fdb66c933cdd86b8fcee1df4f2c91e
NongPhloeut19 <phloeutnong@gmail.com>
2023-07-04 16:42:33 +0700

    [G3SMS-127] creat login account

--- backend/config/app.php
+++ backend/config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /// Application Service Providers for authentication
+        // Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
