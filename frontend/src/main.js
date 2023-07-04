8484dc69d801f52a1682d8357d3363a043a7d326
Sreypok-D <sreypokdoem@gmail.com>
2023-07-04 19:15:14 +0700

    G3SMS-129 <create form for create user>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import CreateUserForm from './views/Dashboard/CreateUserForm.vue'
 
-createApp(App).use(router).mount('#app')
+loadFonts()
+const app = createApp(App)
+app.component('create-user-form' , CreateUserForm)
+app
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
