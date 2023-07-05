7217f48428ec2be6bf9359d0fdc6e3dc88bb7101
NAVY.RORN <NAVY.RORN@PNC.LAN>
2023-07-05 08:42:12 +0700

    [G3SMS-182] create form for forgot and set up password

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,15 +9,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import router from './router'
 import {createRouter , createWebHistory } from 'vue-router'
+
 import CreateUserForm from './views/Dashboard/CreateUserForm.vue'
 import ResetPassword from './views/Login/ResetPassword.vue'
+import ForgotPassword from './views/Login/ForgotPassword.vue'
+import SetupPassword from './views/Login/SetupPassword.vue'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 const router = createRouter({
     history:createWebHistory(),
     routes:[
         { path: '/createpage' , component: CreateUserForm},
-        { path: '/resetpasspage' , component: ResetPassword}
+        { path: '/resetpasspage' , component: ResetPassword},
+        { path: '/forgotpassword' , component: ForgotPassword},
+        { path: '/setuppassword' , component: SetupPassword}
     ]
 })
 createApp(App).use(router).mount('#app')
---
+++ frontend/src/views/Login/ForgotPassword.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container d-flex flex-column">
+    <div class="row align-items-center min-vh-100">
+      <div class="div col-12 col-md-8 col-lg-4">
+        <img src="../../assets/resetpassword.jpg" />
+        <div>
+          <form class="rounded bg-white shadow p-7 main shadow-lg">
+            <h3 class="text-dark fw-bolder fs-4 mb-2">Forget Password ?</h3>
+
+            <div class="fw-normal text-muted mb-4">
+              Enter your email to reset your password.
+            </div>
+
+            <div class="form-floating mb-3">
+              <input
+                type="email"
+                class="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput">Email address</label>
+            </div>
+
+            <button type="submit" class="btn btn-primary submit_btn my-4">
+              Submit
+            </button>
+            <button
+              type="submit"
+              class="btn btn-secondary submit_btn my-4 ms-3"
+            >
+              Cancel
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+    
+</script>
+
+<style scoped>
+img {
+  width: 120%;
+}
+.main {
+  background: white;
+  width: 350%;
+  padding: 30px;
+  border: solid 1px rgb(241, 235, 225);
+}
+h3 {
+  text-align: center;
+}
+button {
+  width: 30%;
+}
+.div {
+  display: flex;
+  margin-left: 90px;
+}
+</style>
