5cf8213ea5b390b0bb33e1d7d5ed62617bbb2598
Sreypok-D <sreypokdoem@gmail.com>
2023-07-05 08:31:32 +0700

    [G3SMS-52] update create user back-end

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // ***Route User***
-Route::resource("/users", UserController::class);
+Route::resource("users", UserController::class);
