9dcaf6ae07c762d5484e1229be2ea5c0cb0615a4
Sreypok-D <sreypokdoem@gmail.com>
2023-07-06 16:31:03 +0700

    [G3SMS-185] create chart for student attendance

--- frontend/src/views/Director/DirectorDashboard.vue
+++ frontend/src/views/Director/DirectorDashboard.vue
@@ -1,78 +1,93 @@
 <template>
-    <main class="py-6 bg-surface-secondary">
-        <div class="container-fluid">
-
-            <div class="row g-6 mb-6">
-                <div class="col-xl-3 col-sm-6 col-12">
-                    <div class="card shadow border-0">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col">
-                                    <span class="h6 font-semibold text-muted text-sm d-block mb-2">Students</span>
-                                    <span class="h3 font-bold mb-0">2600</span>
-                                </div>
-                                <div class="col-auto">
-                                    <div class="icon icon-shape bg-tertiary text-white text-lg rounded-circle">
-                                        <i class="bi bi-people"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  <main class="py-6 bg-surface-secondary  ">
+    <div class="container-fluid ">
+      <div class="row g-6 mb-6 ">
+        <div class="col-xl-3 col-sm-6 col-12 ">
+          <div class="card shadow border-0 bg-gray-200">
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <span class="h6 font-semibold text-muted text-sm d-block mb-2"
+                    >Students</span
+                  >
+                  <span class="h3 font-bold mb-0">2600</span>
                 </div>
-                <div class="col-xl-3 col-sm-6 col-12">
-                    <div class="card shadow border-0">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col">
-                                    <span class="h6 font-semibold text-muted text-sm d-block mb-2">Teachers</span>
-                                    <span class="h3 font-bold mb-0">215</span>
-                                </div>
-                                <div class="col-auto">
-                                    <div class="icon icon-shape bg-primary text-white text-lg rounded-circle">
-                                        <i class="bi bi-people"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-auto">
+                  <div
+                    class="icon icon-shape bg-tertiary text-white text-lg rounded-circle"
+                  >
+                    <i class="bi bi-people"></i>
+                  </div>
                 </div>
-                <div class="col-xl-3 col-sm-6 col-12">
-                    <div class="card shadow border-0">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col">
-                                    <span class="h6 font-semibold text-muted text-sm d-block mb-2">Class</span>
-                                    <span class="h3 font-bold mb-0">40</span>
-                                </div>
-                                <div class="col-auto">
-                                    <div class="icon icon-shape bg-info text-white text-lg rounded-circle">
-                                        <i class="bi bi-building"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-sm-6 col-12">
-                    <div class="card shadow border-0">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col">
-                                    <span class="h6 font-semibold text-muted text-sm d-block mb-2">Libary</span>
-                                    <span class="h3 font-bold mb-0">2</span>
-                                </div>
-                                <div class="col-auto">
-                                    <div class="icon icon-shape bg-warning text-white text-lg rounded-circle">
-                                        <i class="bi bi-book"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
-            <div class="card shadow border-0 mb-7">
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12">
+          <div class="card shadow border-0 bg-gray-200">
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <span class="h6 font-semibold text-muted text-sm d-block mb-2"
+                    >Teachers</span
+                  >
+                  <span class="h3 font-bold mb-0">215</span>
+                </div>
+                <div class="col-auto">
+                  <div
+                    class="icon icon-shape bg-primary text-white text-lg rounded-circle"
+                  >
+                    <i class="bi bi-people"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12">
+          <div class="card shadow border-0 bg-gray-200">
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <span class="h6 font-semibold text-muted text-sm d-block mb-2"
+                    >Class</span
+                  >
+                  <span class="h3 font-bold mb-0">40</span>
+                </div>
+                <div class="col-auto">
+                  <div
+                    class="icon icon-shape bg-info text-white text-lg rounded-circle"
+                  >
+                    <i class="bi bi-building"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12">
+          <div class="card shadow border-0 bg-gray-200">
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <span class="h6 font-semibold text-muted text-sm d-block mb-2"
+                    >Libary</span
+                  >
+                  <span class="h3 font-bold mb-0">2</span>
+                </div>
+                <div class="col-auto">
+                  <div
+                    class="icon icon-shape bg-warning text-white text-lg rounded-circle"
+                  >
+                    <i class="bi bi-book"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+      <!-- <div class="card shadow border-0 mb-7">
                 <div class="card-header">
                     <h5 class="mb-0">STUDENT LIST</h5>
                 </div>
@@ -129,11 +144,43 @@
                 <div class="card-footer border-0 py-5">
                     <span class="text-muted text-sm">Showing 10 items out of 250 results found</span>
                 </div>
-            </div>
+            </div> -->
+        <div class="chart">
+            <Bar
+            id="my-chart-id"
+            :options="chartOptions"
+            :data="chartData"
+        />
         </div>
-    </main>
+        </div>
+ 
+
+  </main>
 </template>
 
+<script >
+    import { Bar } from 'vue-chartjs'
+    import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+    ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [ { data: [40, 20, 12,45,32,22,11,33,44,98,55,43] } ]
+    },
+    chartOptions: {
+        backgroundColor: '#3388FF',
+        responsive: true
+      }
+    }
+  }
+}
+</script>
 <style>
 @import url(https://unpkg.com/@webpixels/css@1.1.5/dist/index.css);
 
