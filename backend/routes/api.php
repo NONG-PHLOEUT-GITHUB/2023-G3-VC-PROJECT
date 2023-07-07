db0f54e6e0153d6ca427558e5131dc08f0764e6c
NongPhloeut19 <phloeutnong@gmail.com>
2023-07-07 08:04:35 +0700

    [G3SMS-33] manage login in app

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,7 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-<<<<<<< HEAD
 Route::post('/login', 'LoginController@login');
 
 // ***Route User***
@@ -31,11 +31,12 @@ Route::resource("users", UserController::class);
 Route::post('/login',[LoginController::class,'login'])
                 ->middleware('guest')
                 ->name('login');
+Route::post('/logout',[LogoutController::class,'logout'])
+                ->middleware('guest')
+                ->name('logout');
 
 Route::get('/users',[UserController::class,'index']);
 Route::post('/users',[UserController::class,'store']);
 
-=======
 
 Route::resource('users' , UserController::class);
->>>>>>> ad1f8c0069b452d9d8f13f28aec27377e23a625a
