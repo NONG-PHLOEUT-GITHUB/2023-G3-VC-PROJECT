6f28595c09239204453b753b97d2ffd119ff2dbe
Sreypok-D <sreypokdoem@gmail.com>
2023-07-08 11:11:22 +0700

    [G3SMS-86] update table user

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -58,7 +58,6 @@ class User extends Authenticatable
     public static function store($request, $id = null)
     {
         $users = $request->only(
-            'user_id',
             'first_name',
             'last_name',
             'gender',
