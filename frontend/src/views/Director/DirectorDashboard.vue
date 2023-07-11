0c9f5484f055b649fcd75b3cf10f8e264de1edad
rienleam <rienleam@gmail.com>
2023-07-11 16:42:37 +0700

    [G3SMS-111] create controller to show monthly attendacne

--- frontend/src/views/Director/DirectorDashboard.vue
+++ frontend/src/views/Director/DirectorDashboard.vue
@@ -87,72 +87,26 @@
           </div>
         </div>
     </div>
-      <!-- <div class="card shadow border-0 mb-7">
-                <div class="card-header">
-                    <h5 class="mb-0">STUDENT LIST</h5>
-                </div>
-                <div class="table-responsive">
-                    <table class="table table-hover table-nowrap">
-                        <thead class="thead-light">
-                            <tr>
-                                <th scope="col">Name</th>
-                                <th scope="col">Date</th>
-                                <th scope="col">Class</th>
-                                <th scope="col">Offer</th>
-                                <th scope="col">Meeting</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <img alt="..."
-                                        src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80"
-                                        class="avatar avatar-sm rounded-circle me-2">
-                                    <a class="text-heading font-semibold" href="#">
-                                        Robert Fox
-                                    </a>
-                                </td>
-                                <td>
-                                    Feb 15, 2021
-                                </td>
-                                <td>
-                                    <img alt="..." src="https://preview.webpixels.io/web/img/other/logos/logo-1.png"
-                                        class="avatar avatar-xs rounded-circle me-2">
-                                    <a class="text-heading font-semibold" href="#">
-                                        Dribbble
-                                    </a>
-                                </td>
-                                <td>
-                                    $3.500
-                                </td>
-                                <td>
-                                    <span class="badge badge-lg badge-dot">
-                                        <i class="bg-success"></i>Scheduled
-                                    </span>
-                                </td>
-                                <td class="text-end">
-                                    <a href="#" class="btn btn-sm btn-neutral">View</a>
-                                    <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
-                                        <i class="bi bi-trash"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="card-footer border-0 py-5">
-                    <span class="text-muted text-sm">Showing 10 items out of 250 results found</span>
-                </div>
-            </div> -->
-        <div class="chart">
-            <Bar
-            id="my-chart-id"
-            :options="chartOptions"
-            :data="chartData"
+    <h3>REPORTS</h3>
+    <div class="charts d-flex" style="width: 100%;">
+      <div class="chart" style="width: 50%;">
+        <Bar
+        id="my-chart-id"
+        :options="chartOptions"
+        :data="chartData"
         />
-        </div>
-        </div>
+        <h5 class="text-center">STUDENT ATTENDANCE</h5>
+      </div>
+      <div class="chart" style="width: 50%;">
+        <Bar
+        id="my-chart-id"
+        :options="chartOptions"
+        :data="chartData1"
+        />
+        <h5 class="text-center">FAILED STUDENT</h5>
+      </div>
+    </div>
+    </div>
  
 
   </main>
@@ -171,10 +125,18 @@ export default {
     return {
       chartData: {
         labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [ { data: [40, 20, 12,45,32,22,11,33,44,98,55,43] } ]
+        datasets: [ { 
+          label: "Attendance average", 
+          data: [40, 20, 12, 45, 32, 22, 11, 33, 44, 58, 55, 43] } ],
+    },
+      chartData1: {
+        labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [ { 
+          label: "Average of failed students",
+          data: [10, 20, 12, 45, 32, 22, 11, 33, 44, 69, 55, 43] } ],
     },
     chartOptions: {
-        backgroundColor: '#3388FF',
+        backgroundColor: '#FFA500',
         responsive: true
       }
     }
