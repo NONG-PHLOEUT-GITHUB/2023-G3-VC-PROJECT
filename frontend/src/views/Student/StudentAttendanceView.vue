01278b7540dbfdc85a179f5281d5113707295839
linnamuth <muthlinna73gmail.com>
2023-07-27 16:44:37 +0700

    [G3SMS-236]create controller sent message telagram

--- frontend/src/views/Student/StudentAttendanceView.vue
+++ frontend/src/views/Student/StudentAttendanceView.vue
@@ -1,71 +1,62 @@
 <template>
-  <student-dashboard />
-  <h3 class="title mb-2">MY ATTENDANCES RECORDS</h3>
-  <v-card class="card mt-3">
-    <v-tabs v-model="tab" bg-color="deep-purple-darken-4" center-active>
-      <v-tab
-        v-for="(month, index) in monthsWithAttendance"
-        :key="month"
-        :value="index + 1"
-      >
-        {{ month }}
-      </v-tab>
-    </v-tabs>
-  </v-card>
-  <v-card-text class="card">
-    <v-window v-model="tab">
-      <v-window-item
-        v-for="(month, index) in monthsWithAttendance"
-        :key="month"
-        :value="index + 1"
-      >
-        <div class="table-responsive">
-          <table class="table table-hover table-nowrap mt-2">
-            <thead class="bg-primary">
-              <tr>
-                <th scope="col" class="text-sm text-light">Date</th>
-                <th scope="col" class="text-sm text-light">Status</th>
-                <th scope="col" class="text-sm text-light">Reason</th>
-              </tr>
-            </thead>
-            <tbody>
-              <template v-if="getAttendanceByMonth(index + 1).length === 0">
-                <tr>
-                  <td colspan="3" class="text-center text-md text-dark">
-                    There is no attendance yet in this month
-                  </td>
-                </tr>
-              </template>
-              <template v-else>
-                <tr
-                  v-for="attendance in getAttendanceByMonth(index + 1)"
-                  :key="attendance.id"
-                  class="border-2-dark"
-                >
-                  <td class="text-sm text-black">{{ attendance.date }}</td>
-                  <td class="text-sm text-red">{{ attendance.status }}</td>
-                  <td class="text-sm text-black">{{ attendance.reason }}</td>
-                </tr>
-              </template>
-            </tbody>
-          </table>
-        </div>
-      </v-window-item>
-    </v-window>
-  </v-card-text>
+  <student-dashboard></student-dashboard>
+  <div>
+    <h3 class="text">ABSENCE MONTHLY RECORD</h3>
+    <v-card class="card mt-3">
+      <v-tabs v-model="tab" bg-color="primary">
+        <v-tab v-for="(month, index) in months" :key="index" :value="index + 1">
+          {{ month }}
+        </v-tab>
+      </v-tabs>
+      <v-card-text class="container">
+        <v-window v-model="tab">
+          <v-window-item
+            v-for="(month, index) in months"
+            :key="index"
+            :value="index + 1"
+          >
+            <div class="table-responsive">
+              <table class="table table-hover table-nowrap">
+                <thead>
+                  <tr>
+                    <th scope="col" class="text-sm text-primary">Date</th>
+                    <th scope="col" class="text-sm text-primary">Status</th>
+                    <th scope="col" class="text-sm text-primary">Reason</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    v-for="(detail, index) in filteredAttendanceDetails"
+                    :key="index"
+                  >
+                    <td class="text-sm text-black">{{ detail.date }}</td>
+                    <td class="text-sm text-black">
+                      {{ detail.status }}
+                    </td>
+                    <td class="text-sm text-black">{{ detail.reason }}</td>
+                  </tr>
+                  <tr v-if="filteredAttendanceDetails.length === 0">
+                    <td colspan="5" class="text-center text-base text-dark">
+                      No absence records for this month.
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </v-window-item>
+        </v-window>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
-import http from "@/htpp.common";
-
+import http from "@/htpp.common.js";
 export default {
-  data: () => ({
-    tab: null,
-    attendances: [],
-  }),
-  computed: {
-    months() {
-      return [
+  data() {
+    return {
+      tab: 1,
+      months: [
         "January",
         "February",
         "March",
@@ -78,63 +69,48 @@ export default {
         "October",
         "November",
         "December",
-      ];
-    },
-    attendancesByMonth() {
-      const attendancesByMonth = {};
-      for (const attendance of this.attendances) {
-        const month = new Date(attendance.date).getMonth() + 1;
-        if (!attendancesByMonth[month]) {
-          attendancesByMonth[month] = [];
-        }
-        attendancesByMonth[month].push(attendance);
-      }
-      return attendancesByMonth;
-    },
-
-    monthsWithAttendance() {
-      const monthsWithAttendance = [];
-      for (let i = 1; i <= 12; i++) {
-        if (this.attendancesByMonth[i]?.length) {
-          monthsWithAttendance.push(this.months[i - 1]);
-        } else {
-          monthsWithAttendance.push(
-            this.months[i - 1] 
-          );
-        }
-      }
-      return monthsWithAttendance;
+      ],
+      attendanceDetails: [],
+    };
+  },
+  computed: {
+    filteredAttendanceDetails() {
+      return this.attendanceDetails.filter(
+        (detail) => parseInt(detail.date.split("-")[1]) === this.tab
+      );
     },
   },
   methods: {
-    getAttendance() {
-      http.get("/v1/auth/user").then((response) => {
-        this.attendances = response.data.data.attendances;
-        console.log(this.attendances);
-      });
-    },
-    getAttendanceByMonth(month) {
-      return this.attendances.filter((attendance) => {
-        const attendanceMonth = new Date(attendance.date).getMonth() + 1;
-        return attendanceMonth === month;
-      });
+    getAttendanceDetails() {
+      http
+        .get("/v1/auth/user")
+        .then((response) => {
+          this.attendanceDetails = response.data.data.attendances;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
   },
   mounted() {
-    this.getAttendance();
+    this.getAttendanceDetails();
   },
 };
 </script>
 
 <style scoped>
-@import "~vuetify/dist/vuetify.css";
 .card {
   margin-left: 17%;
 }
+.text {
+  margin-left: 47%;
+  padding: 1%;
+}
+.container {
+  padding: 5px;
+}
+
 .table th {
   font-size: 20px;
-}
-.title {
-  margin-left: 18%;
 }
 </style>
