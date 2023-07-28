c5758c0301fb4b1cf8967ea8029ac8238816286f
NAVY.RORN <NAVY.RORN@PNC.LAN>
2023-07-28 20:31:56 +0700

    [G3SMS-244] count total of class

--- backend/app/Http/Controllers/ClassRoomController.php
+++ backend/app/Http/Controllers/ClassRoomController.php
@@ -98,23 +98,25 @@ class ClassRoomController extends Controller
         }
     }
     public function getStudentsInClass(string $id)
-{
-    $classRooms = ClassRoom::where('id', $id)
-        ->whereHas('students', function ($query) {
-            $query->where('role', 3);
-        })
-        ->with(['students' => function ($query) {
-            $query->withCount('roleAttendances');
-            $query->orderByDesc('role_attendances_count');
-        }])
-        ->get();
+    {
+        $classRooms = ClassRoom::where('id', $id)
+            ->whereHas('students', function ($query) {
+                $query->where('role', 3);
+            })
+            ->with([
+                'students' => function ($query) {
+                    $query->withCount('roleAttendances');
+                    $query->orderByDesc('role_attendances_count');
+                }
+            ])
+            ->get();
 
-    if ($classRooms->isEmpty()) {
-        return "not found";
-    } else {
-        return response()->json(['success' => true, 'data' => $classRooms], 200);
+        if ($classRooms->isEmpty()) {
+            return "not found";
+        } else {
+            return response()->json(['success' => true, 'data' => $classRooms], 200);
+        }
     }
-}
 
     public function getClassNameTeacherId(string $className)
     {
@@ -124,17 +126,30 @@ class ClassRoomController extends Controller
             })
             ->with('students')
             ->get();
-    
+
         if ($classRooms->isEmpty()) {
             return "not found";
         } else {
             return response()->json(['success' => true, 'data' => $classRooms], 200);
         }
     }
-    
+
     public function getTotalOfClass()
     {
         $classrooms = ClassRoom::count();
         return response()->json(['success' => true, 'data' => $classrooms], 200);
+    }
+    /**
+     * Count of each class.
+     */
+    public function getTotalOfEachClass()
+    {
+        $classroomCounts = [
+            '10' => ClassRoom::where('class_name', 'like', '10%')->count(),
+            '11' => ClassRoom::where('class_name', 'like', '11%')->count(),
+            '12' => ClassRoom::where('class_name', 'like', '12%')->count(),
+        ];
+
+        return response()->json(['success' => true, 'data' => $classroomCounts], 200);
     }
 }
